ba2492cde0bb02c8144637c23ba78d6e1ff347c0
Noah Van Gorden <nvgnoah@gmail.com>
2015-03-09 22:42:41 -0700

    move email js to external file

---
+++ js/main.js
@@ -0,0 +1,4 @@
+var userName = "noahvangorden";
+var hostName = "gmail.com";
+var linkText = userName + "@" + hostName;
+document.write("<a href='" + "mail" + "to:" + userName + "@" + hostName + "'>" + linkText + "</a>");
